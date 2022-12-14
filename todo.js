5e30be0cc4ec05d9fd9f447cedd1bfe34a007905
MhsMiraz <mhsmiraz@gmail.com>
2022-12-14 18:57:45 +0600

    delete button added

--- todo.js
+++ todo.js
@@ -9,10 +9,17 @@ document.getElementById("input-btn").addEventListener("click", function () {
     <th scope="row">${count}</th>
     <td>${inputValue}</td>
     <td>
-    <button class="btn btn-primary">Delete</button>
+    <button class="btn btn-primary delete-btn">Delete</button>
     <button class="btn btn-success">Done</button>
     </td>
     `;
     bodyContainer.appendChild(createTable);
     document.getElementById("input-value").value = "";
+
+    const deleteBtn = document.getElementsByClassName("delete-btn");
+    for(let button of deleteBtn){
+        button.addEventListener("click", function(event){
+            event.target.parentNode.parentNode.style.display = "none";
+        })
+    }
 })
